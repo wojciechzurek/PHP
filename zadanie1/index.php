46394bbec08a9b7e0533ceb3cdd98c2a84b67dd6
wojciechzurek <wojtas.zurek5@gmail.com>
2017-04-19 13:44:13 +0200

    zadanie1

---
+++ zadanie1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Strona Xampik</title>
+		<meta charset="UTF-8"></meta>
+	</head>
+	<body>
+		<form action="test.php" method="get">
+			Imię: <input type="text" name="form_imie"><br>
+			Nazwisko: <input type="text" name="form_nazwisko"><br>
+			<input type="submit">
+		</form>
+	</body>
+</html>
---
+++ zadanie1/test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Strona Xa PHP</title>
+		<meta charset="UTF-8"></meta>
+	</head>
+	<body>
+		Twoje imię: <?php echo $_GET["form_imie"]; ?><br>
+		Twoje nazwisko: <?php echo $_GET["form_nazwisko"]; ?>
+	</body>
+</html>
